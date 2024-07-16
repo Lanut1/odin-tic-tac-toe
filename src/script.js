64e5d9ee72ba6b18402812013f2dda31f615c1fd
Lanut1 <anna.tabakova27@gmail.com>
2024-07-16 21:28:06 +0200

    First version of UI Tic Tac Toe project, restart game bugs remain

--- src/script.js
+++ src/script.js
@@ -6,9 +6,13 @@ const Cell = (function() {
             value = player;
         }
 
+        const clearCell = () => {
+            value = 0;
+        }
+
         const getValue = () => value;
 
-        return { addToken, getValue };
+        return { addToken, clearCell, getValue };
     };
 })();
 
@@ -30,15 +34,20 @@ const Gameboard = (function () {
     const board = [];
     const sides = 3;
 
-    for (let i = 0; i < sides; i++) {
-        board[i] = [];
-        for (let j = 0; j < sides; j++) {
-            board[i].push(Cell());
+    const makeBoard = () => {
+        for (let i = 0; i < sides; i++) {
+            board[i] = [];
+            for (let j = 0; j < sides; j++) {
+                board[i].push(Cell());
+            }
         }
     }
 
-    const makeBoard = () => board;
+    const clearBoard = () => {
+        board.map(row => row.map(cell => cell.clearCell()));
+    }
 
+    // I think i dont need this for UI version...
     const printBoardValues = () => {
         const boardValues = board.map((row) => row.map((cell) => cell.getValue()));
         console.table(boardValues);
@@ -59,20 +68,15 @@ const Gameboard = (function () {
         }
     }
 
-    return { makeBoard, printBoardValues, placeToken };
+    return { makeBoard, clearBoard, printBoardValues, placeToken };
 }) ();
 
 
 const GameControler = (function () {
-    return function(player1Name, player1Token, player2Name, player2Token) {
+    return function(players) {
         const sides = 3;
         Gameboard.makeBoard();
         Gameboard.printBoardValues();
-
-        const players = [
-            Player(player1Name, player1Token),
-            Player(player2Name, player2Token)
-        ];
 
         let activePlayer = players[0];
 
@@ -105,29 +109,71 @@ const GameControler = (function () {
 
         const checkFullBoard = (roundValues) => roundValues.every(roundRow => roundRow.every(roundCell => roundCell !== 0));
 
-        const playRound = () => {
-            try {
-                let row = prompt(`${getActivePlayer().getPlayerName()}'s turn! Please enter row:`);
-                let column = prompt(`${getActivePlayer().getPlayerName()}'s turn! PLease enter column:`);
+        const resetGame = () => {
+            const cells = document.querySelectorAll(".board-cell");
+            cells.forEach(cell => cell.innerText = "");
 
-                Gameboard.placeToken(parseInt(row), parseInt(column), getActivePlayer().getPlayerToken());
+            const gameBoard = document.querySelector(".game-board");
+            gameBoard.style.display = "none";
+
+            const message = document.querySelector(".message-container");
+            message.style.display = "none";
+
+            const firstPlayerInfo = document.querySelector(".first-player-info");
+            firstPlayerInfo.style.display = "none";
+
+            const secondPlayerInfo = document.querySelector(".second-player-info");
+            secondPlayerInfo.style.display = "none";
+
+            const startGameButton = document.querySelector("#start-button");
+            const firstPlayerForm = document.querySelector(".first-player");
+            firstPlayerForm.reset();
+            const secondPlayerForm = document.querySelector(".second-player");
+            secondPlayerForm.reset();
+
+            firstPlayerForm.style.display = "block";
+            secondPlayerForm.style.display = "block";
+            startGameButton.style.display = "block";
+
+        }
+
+        const showResultDialog = (message) => {
+            const dialogWindow = document.querySelector("#result-dialog");
+            const resultMessage = document.querySelector("#result-message");
+            const restartButton = document.querySelector("#restart-game");
+            
+            resultMessage.innerText = message;
+            dialogWindow.showModal();
+
+            restartButton.addEventListener("click", function() {
+                dialogWindow.close();
+                resetGame();
+                PlayGame();
+            })
+        }
+
+        const playRound = (row, column) => {
+            const messageContainer = document.querySelector(".message-container");
+            try {
+                Gameboard.placeToken(row, column, getActivePlayer().getPlayerToken());
                 
                 let roundValues = Gameboard.printBoardValues();
 
                 if (checkWinRow(roundValues) || checkWinColumn(roundValues) || checkWinDiagonal(roundValues)) {
-                    console.log(`${getActivePlayer().getPlayerName()} wins!`);
+                    showResultDialog(`${getActivePlayer().getPlayerName()} wins!`);
                     return false; // game is over
                 } else if (checkFullBoard(roundValues)) {
-                    console.log("Tie! Board is full!");
+                    showResultDialog("Tie! Board is full!");
                     return false; // game is over
                 }
 
                 switchPlayer();
+                messageContainer.innerText = `${getActivePlayer().getPlayerName()}'s turn!`;
                 return true;
                 
             } catch (error) {
-                console.error(error.message);
-                return true;
+                messageContainer.innerText = error.message;
+                throw new Error("error");
             }
         }
 
@@ -138,36 +184,84 @@ const GameControler = (function () {
 
 const PlayGame = (function () {
     return function () {
-        console.log("Welcome to Tic Tac Toe!");
-        console.log("To make a move, you'll be prompted to enter players' names, tokens, the row and column numbers.");
-        console.log("The board is numbered from 0 to 2 for both rows and columns.");
+        const firstPlayerForm = document.querySelector(".first-player");
+        const secondPlayerForm = document.querySelector(".second-player");
+        const isFirstPlayerFormValid = firstPlayerForm.checkValidity();
+        const isSecondPlayerFormValid = secondPlayerForm.checkValidity();
 
-        setTimeout(() => {
-            let firstPlayerName = prompt("Enter first player name");
-            let firstPlayerToken = prompt("Enter first player token");
-
-            let secondPlayerName = prompt("Enter second player name");
-            let secondPlayerToken = prompt("Enter second player token");
-
-            const game = GameControler(firstPlayerName, firstPlayerToken, secondPlayerName, secondPlayerToken);
-
-            let continueGame = true;
-
-            while (continueGame) {
-                continueGame = game.playRound();
-            }
-
-            let playAgain = prompt("Do you want to play again? yes/no").toLowerCase();
-
-            if (playAgain === "yes") {
-                console.clear();
-                PlayGame();
-            } else {
-                console.log("Thanks for playing!");
-            }
-        }, 5000);
+        if (!isFirstPlayerFormValid) {
+            firstPlayerForm.reportValidity();
+            return;
+        }else if (!isSecondPlayerFormValid) {
+            secondPlayerForm.reportValidity();
+            return;
         }
+
+        const firstPlayerName = document.querySelector("#first-player-name").value;
+        const firstPlayerToken = document.querySelector('input[name="coffee-icons"]:checked').value;
+        const secondPlayerName = document.querySelector("#second-player-name").value;
+        const secondPlayerToken = document.querySelector('input[name="tea-icons"]:checked').value;
+
+        firstPlayerForm.style.display = "none";
+        secondPlayerForm.style.display = "none";
+        startGameButton.style.display = "none";
+
+        const firstPlayerInfo = document.querySelector(".first-player-info");
+        const gameBoard = document.querySelector(".game-board");
+        const secondPlayerInfo = document.querySelector(".second-player-info");
+        const messageContainer = document.querySelector(".message-container");
+
+        const firstNameContainer = document.querySelector(".first-player-name-container");
+        firstNameContainer.innerText = firstPlayerName;
+        const firstTokenImage = document.querySelector(".first-player-token-image");
+        firstTokenImage.src = `src/${firstPlayerToken}.png`;
+        firstPlayerInfo.style.display = "block";
+
+        gameBoard.style.display = "grid";
+
+        const secondNameContainer = document.querySelector(".second-player-name-container");
+        secondNameContainer.innerText = secondPlayerName;
+        const secondTokenImage = document.querySelector(".second-player-token-image");
+        secondTokenImage.src = `src/${secondPlayerToken}.png`;
+        secondPlayerInfo.style.display = "block";
+
+        messageContainer.style.display = "block";
+
+        const players = [
+            Player(firstPlayerName, firstPlayerToken),
+            Player(secondPlayerName, secondPlayerToken)
+        ];
+
+        const game = GameControler(players);
+
+        let continueGame = true;
+
+        messageContainer.innerText = `${game.getActivePlayer().getPlayerName()}'s turn! Place your token!`;
+
+        document.querySelectorAll(".board-cell").forEach(cell => {
+            cell.addEventListener("click", function() {
+                if (continueGame) {
+                    const row = parseInt(this.dataset.row);
+                    const column = parseInt(this.dataset.column);
+                    const currentPlayer = game.getActivePlayer().getPlayerToken();
+                    try {
+                        continueGame = game.playRound(row, column);
+                        const img = document.createElement("img");
+                        img.src = `src/${currentPlayer}.png`;
+                        img.classList.add("gameboard-token-image");
+                        this.appendChild(img);
+                    } catch (error) {
+                    }
+                }
+            })
+        })
+    }
 }) ();
 
 
-PlayGame();
+const startGameButton = document.querySelector("#start-button");
+
+startGameButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    PlayGame();
+})
